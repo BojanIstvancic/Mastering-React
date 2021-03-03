58a07c5b978d49d540701eefe1816b14eae695fd
BojanIstvancic <bojan.istvancic@gmail.com>
2021-03-03 10:27:44 +0100

    useEffect - practice

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,16 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 /* useState - is the function for setting the state */
+/* useEffect - */
 import CreateTweet from "./components/CreateTweet";
 import TweetList from "./components/TweetList";
 
 function App() {
   const name = "Bokisa";
-
   // State
   const [devName, setDevName] = useState("HTML CSS Boxton");
   // [ variableName, variableSetter] = useState('Default Value)
   const [textInput, setTextInput] = useState("");
   const [tweets, setTweets] = useState([]);
+
+  useEffect(() => {
+    console.log("Tweets is changed");
+  }, [tweets]);
+  /* 
+  this function starts runing everytime when the app is mounted (load/refresh - fetch data)
+  1 - function
+  2 - we can also define when the useEffect activates -> which state should change to change to activate useEffect
+  we can use this also for suggestion list: as we are searching, we update the suggestion list data
+  */
 
   const sayHelloHandler = () => {
     setDevName("Boxton React Boxton");
